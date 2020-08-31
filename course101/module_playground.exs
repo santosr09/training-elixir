9cec43911a9520df75a4b426cb63a09a30ad0b1e
santosr09 <santos.r09@gmail.com>
2020-08-30 22:04:15 -0500

    Notes file added; adding directives training

--- course101/module_playground.exs
+++ course101/module_playground.exs
@@ -1,5 +1,46 @@
 defmodule ModulePlayground do
-    def say_here do
-        IO.puts "I'm Here"
-    end
+   #2
+   #import IO
+   import IO, only: [puts: 1]
+   import Kernel, except: [inspect: 1]
+
+   #Alias
+   #alias ModulePlayground.Misc.Util.Math
+
+   #Alias with custom alias
+   alias ModulePlayground.Misc.Util.Math, as: MyMath
+
+   require Integer
+
+   def say_here do
+      #1
+      #IO.puts "I'm Here"
+      
+      #2
+      #puts "I'm Here with import"
+
+      #3
+      inspect "I'm Here, inspect function"
+   end
+
+   def inspect(param1) do
+       puts "Starting Output"
+       puts param1
+       puts "Ending output"
+   end
+
+   def print_sum do
+       #ModulePlayground.Misc.Util.Math.add(2,6)
+       
+       #Alias
+       #Math.add(8,6)
+
+       #Custom Alias
+       MyMath.add(3,7)
+   end
+
+   def print_is_even(num) do
+      puts "Is #{num} even? #{Integer.is_even(num)}"
+   end
+
 end
