60c96736c9603fb009a9ea9558c5dd81f8da107b
santosr09 <santos.r09@gmail.com>
2020-09-01 08:39:01 -0500

    Matching Example

--- course102_functions/sample_enum.exs
+++ course102_functions/sample_enum.exs
@@ -1,6 +1,36 @@
 defmodule Sample.Enum do
-    #Your tasks here!
-    def first(list) do
-        hd(list)
-    end
+    #Matching when list is empty
+    #def first([]) do
+    #    nil
+    #end
+    
+    #1
+    #Matching when list has elements
+    #def first(list) do
+    #    hd(list)
+    #end
+
+    #2
+    #WARNING: variable tail unused
+    #def first([head | tail]) do
+    #3
+    #def first([head | _]) do
+    #    head
+    #end
+
+    #Shortcut when a function is simple:
+    def first([]), do: nil
+    def first([head | _]), do: head
+
+    #4
+    #Example of pattern matching
+   def some_func(quantity, {_, _, price}) do
+      quantity * price
+   end
+
+   #Less legible
+   def some_func(quantity, book) do
+      quantity * elem(book, 2)
+   end
+
 end
