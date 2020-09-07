d05afb0c53281dbd1f2c287e912f651b3eaa2001
santosr09 <santos.r09@gmail.com>
2020-09-06 22:07:18 -0500

    Reading a file

--- course104_first_app/application_tweet/lib/application_tweet/file_reader.ex
+++ course104_first_app/application_tweet/lib/application_tweet/file_reader.ex
@@ -1,6 +1,9 @@
 defmodule AppTweet.FileReader do
     def get_strings_to_tweet(path) do
         File.read!(path)
-            |> String.split("\n")
+            |> String.split("\n") #Pipe Operator |>
+            |> Enum.map(&String.trim/1)
+            |> Enum.filter(&String.length(&1) <= 140)
+            #|> Enum.filter(fn str -> String.length(str) <= 140 end)
     end
 end
